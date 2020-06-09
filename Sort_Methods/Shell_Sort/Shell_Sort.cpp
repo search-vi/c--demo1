0afb0b080ed25d8b9fc88b17b2818da4dba4e0cc
Free-Geter <1321687936@qq.com>
2020-06-09 23:25:47 +0800

    add test funs

--- Sort_Methods/Shell_Sort/Shell_Sort.cpp
+++ Sort_Methods/Shell_Sort/Shell_Sort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
 
 template <typename T>
@@ -26,11 +27,96 @@ void Shell_Sort(T A[], int n)
     }
 }
 
+template <typename T>
+void shell_sortSp(T array[], int length)
+{
+    int h = 1;
+    while (h < length / 3)
+    {
+        h = 3 * h + 1;
+    }
+    while (h >= 1)
+    {
+        for (int i = h; i < length; i++)
+        {
+            for (int j = i; j >= h && array[j] < array[j - h]; j -= h)
+            {
+                std::swap(array[j], array[j - h]);
+            }
+        }
+        h = h / 3;
+    }
+}
+
+//定义测试时间的函数。
+#include <stdlib.h>
+#include <time.h>
+
+template <class Func, typename T>
+double calc_time(Func f, T R[], int n)
+{
+    clock_t start = clock();
+    f(R, n);
+    clock_t finish = clock();
+    double duration = (double)(finish - start) / CLOCKS_PER_SEC;
+    return duration;
+}
+
+//按照下列方式提供三种不同序列的数据进行测试。
+
+template <class Func, typename T>
+void test_different_sequence(Func f, T R[], int n)
+{
+    sort(R, R + n); //Ascending
+    double d1 = calc_time(f, R, n);
+
+    reverse(R, R + n); //Descending
+    double d2 = calc_time(f, R, n);
+
+    random_shuffle(R, R + n); //Random
+    double d3 = calc_time(f, R, n);
+
+    cout << setw(10) << d1 << setw(10) << d2 << " " << setw(10) << d3 << endl;
+}
+
+//获取初始数据。
+
+int *get_n_data(int n)
+{
+    int *R = new int[n];
+
+    srand((unsigned)time(NULL));
+    generate_n(R, n, rand);
+    return R;
+}
+
+//用不同的数据量进行测试。
+
+template <class Func>
+void test(Func f)
+{
+    static int m[] = {1, 2, 5, 10, 20, 30};
+    int mn = sizeof(m) / sizeof(*m);
+    int N = 1000;
+
+    cout << setw(13) << "\tAscending" << setw(10) << "\tDescending" << setw(8) << "Random" << endl;
+    for (int i = 0; i < mn; ++i)
+    {
+        cout << m[i] << "\t";
+        int n = N * m[i];
+        int *R = get_n_data(n);
+        test_different_sequence(f, R, n);
+        delete[] R;
+    }
+}
+
+//在main进行测试。
+
 int main()
 {
-    int A[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-    int n = sizeof(A) / sizeof(*A);
-    Shell_Sort(A, n);
-    for (int i = 0; i < n; i++)
-        cout << A[i] << '\t';
+    test(shell_sortSp<int>);
+    cout << endl
+         << endl
+         << endl;
+    test(Shell_Sort<int>);
 }
