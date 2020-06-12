1ff5f2d38c9a59349c348d0c8ab2ffcb18227506
Free-Geter <1321687936@qq.com>
2020-06-12 17:54:01 +0800

    add Select_Sort

---
+++ Sort_Methods/Select_Sort/Select_Sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+void Select_Sort(T A[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+            if (A[j] < A[min])
+                min = j;
+        if (min != i)
+            std::swap(A[i], A[min]);
+    }
+}
+
+int main()
+{
+    int A[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int n = sizeof(A) / sizeof(*A);
+    Select_Sort(A, n);
+    for (int i = 0; i < n; i++)
+        cout << A[i] << '\t';
+}
